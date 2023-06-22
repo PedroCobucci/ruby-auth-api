3350cea7b3e5a483c862526beb5c5efb90293d79
Pedro Cobucci <PedroCobucci@gmail.com>
2023-06-22 11:54:28 -0300

    alterações para desafio gooxxy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::API
         include Authorization
 
         before_action :authenticate_with_token
+        before_action :configure_permitted_parameters, if: :devise_controller?
 
         include DeviseTokenAuth::Concerns::SetUserByToken
+
+        def configure_permitted_parameters
+                added_attrs = [:email, :password, :password_confirmation, :nickname, :admin]
+                devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+                devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+        end
 end
